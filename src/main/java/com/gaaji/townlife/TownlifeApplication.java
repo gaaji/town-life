fe9f70415cbe833993f91b429185fc59c75c649b
jinlee0 <wls4370@gmail.com>
2023-02-10 14:38:34 +0900

    GM-299 ÇÁ·ÎÁ§Æ® »ý¼º

---
+++ src/main/java/com/gaaji/townlife/TownlifeApplication.java
@@ -0,0 +1,13 @@
+package com.gaaji.townlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaaji/townlife/TownlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaaji.townlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
