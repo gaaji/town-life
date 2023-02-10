e42bbe07b4a7db29f385ed818d84b343335ee65d
jinlee0 <wls4370@gmail.com>
2023-02-10 14:45:29 +0900

    GM-299 ÇÑ±Û ±úÁü

--- src/main/java/com/gaaji/townlife/TownlifeApplication.java
+++ src/main/java/com/gaaji/townlife/TownlifeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TownlifeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TownlifeApplication.class, args);
 	}
