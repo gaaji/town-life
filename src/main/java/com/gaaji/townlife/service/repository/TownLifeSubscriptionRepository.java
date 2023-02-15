b28ffbfc4b86a6b0265669687f1110baf1fd132f
Seungho <hsh47607@naver.com>
2023-02-15 19:55:23 +0900

    feat: TownLifeSubscription repository 추가

---
+++ src/main/java/com/gaaji/townlife/service/repository/TownLifeSubscriptionRepository.java
@@ -0,0 +1,7 @@
+package com.gaaji.townlife.service.repository;
+
+import com.gaaji.townlife.service.domain.townlife.TownLifeSubscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TownLifeSubscriptionRepository extends JpaRepository<TownLifeSubscription, String> {
+}
