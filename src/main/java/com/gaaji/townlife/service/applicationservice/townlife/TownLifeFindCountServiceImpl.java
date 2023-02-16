e458b13f4c705049a4c2f7afa1e320b580f87522
Seungho <hsh47607@naver.com>
2023-02-16 15:30:30 +0900

    feat: TownLifeCounter 로직 및 레포지토리 구현

--- src/main/java/com/gaaji/townlife/service/applicationservice/townlife/TownLifeFindCountServiceImpl.java
+++ src/main/java/com/gaaji/townlife/service/applicationservice/townlife/TownLifeFindCountServiceImpl.java
@@ -2,6 +2,7 @@ package com.gaaji.townlife.service.applicationservice.townlife;
 
 import com.gaaji.townlife.global.exception.api.ApiErrorCode;
 import com.gaaji.townlife.global.exception.api.ResourceNotFoundException;
+import com.gaaji.townlife.service.domain.townlife.TownLife;
 import com.gaaji.townlife.service.domain.townlife.TownLifeCounter;
 import com.gaaji.townlife.service.repository.TownLifeCounterRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +17,8 @@ public class TownLifeFindCountServiceImpl implements TownLifeFindCountService {
 
     @Override
     @Transactional
-    public TownLifeCounter increaseViewCount(String id) {
-        TownLifeCounter townLifeCounter = townLifeCounterRepository.findById(id)
+    public TownLifeCounter increaseViewCount(TownLife townLife) {
+        TownLifeCounter townLifeCounter = townLifeCounterRepository.findByTownLife(townLife)
                 .orElseThrow(() -> new ResourceNotFoundException(ApiErrorCode.TOWN_LIFE_NOT_FOUND));
 
         return townLifeCounter.view();
