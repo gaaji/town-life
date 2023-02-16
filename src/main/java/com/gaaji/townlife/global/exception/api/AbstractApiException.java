d2b1cb06806654a843a6b07a5605dc75120d88b2
Seungho <hsh47607@naver.com>
2023-02-16 15:25:05 +0900

    feat: Exception에 cause 필드 추가 및 별도 생성자 추가

--- src/main/java/com/gaaji/townlife/global/exception/api/AbstractApiException.java
+++ src/main/java/com/gaaji/townlife/global/exception/api/AbstractApiException.java
@@ -9,12 +9,20 @@ public class AbstractApiException extends RuntimeException implements ErrorCode{
     protected String errorCode;
     protected String errorName;
     protected String errorMessage;
+    protected Throwable cause;
 
     public AbstractApiException (ErrorCode errorCode) {
-
         httpStatus = errorCode.getHttpStatus();
         this.errorCode = errorCode.getErrorCode();
         errorName = errorCode.getErrorName();
         errorMessage = errorCode.getErrorMessage();
+    }
+
+    public AbstractApiException(ErrorCode errorCode, Throwable cause) {
+        httpStatus = errorCode.getHttpStatus();
+        this.errorCode = errorCode.getErrorCode();
+        errorName = errorCode.getErrorName();
+        errorMessage = errorCode.getErrorMessage();
+        this.cause = cause;
     }
 }
