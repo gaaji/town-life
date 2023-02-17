91d5832ed4654c591c455978a8f85d728a7d21af
Seungho <hsh47607@naver.com>
2023-02-17 16:17:21 +0900

    DTO: 동네생활 수정 요청 DTO 정의

--- src/main/java/com/gaaji/townlife/service/controller/townlife/dto/TownLifeModifyRequestDto.java
+++ src/main/java/com/gaaji/townlife/service/controller/townlife/dto/TownLifeModifyRequestDto.java
@@ -1,4 +1,14 @@
 package com.gaaji.townlife.service.controller.townlife.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TownLifeModifyRequestDto {
+    private String title;
+    private String text;
+    private String location;
 }
