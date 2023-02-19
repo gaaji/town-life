b994903b6169b5153c35be89d99a11e7204fd4bb
Seungho <hsh47607@naver.com>
2023-02-19 15:00:37 +0900

    feat: 카테고리 구독 취소 로직 구현

---
+++ src/main/java/com/gaaji/townlife/service/applicationservice/category/CategorySubscriptionServiceImpl.java
@@ -0,0 +1,36 @@
+package com.gaaji.townlife.service.applicationservice.category;
+
+import com.gaaji.townlife.global.exception.api.ApiErrorCode;
+import com.gaaji.townlife.global.exception.api.ResourceNotFoundException;
+import com.gaaji.townlife.service.controller.category.dto.CategorySubscribeDto;
+import com.gaaji.townlife.service.controller.category.dto.CategoryUnsubscribeDto;
+import com.gaaji.townlife.service.domain.category.Category;
+import com.gaaji.townlife.service.domain.category.CategoryUnsubscription;
+import com.gaaji.townlife.service.repository.CategoryRepository;
+import com.gaaji.townlife.service.repository.CategoryUnsubscriptionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategorySubscriptionServiceImpl implements CategorySubscriptionService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryUnsubscriptionRepository categoryUnsubscriptionRepository;
+
+    @Override
+    public void subscribe(String categoryId, CategorySubscribeDto dto) {
+
+    }
+
+    @Override
+    @Transactional
+    public void unsubscribe(String categoryId, CategoryUnsubscribeDto dto) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException(ApiErrorCode.CATEGORY_NOT_FOUND));
+
+        CategoryUnsubscription categoryUnsubscription = categoryUnsubscriptionRepository.save(CategoryUnsubscription.of(dto.getUserId()));
+        categoryUnsubscription.associateCategory(category);
+    }
+}
