b03e1b8d34e79fc2d01f6b0a38b8db8725cfc0b2
Seungho <hsh47607@naver.com>
2023-02-19 15:01:51 +0900

    feat: 요청 시간에 따른 새로운 ULID 생성 메소드를 ULIDGenerator에게 위임

--- src/main/java/com/gaaji/townlife/service/applicationservice/townlife/TownLifeFindServiceImpl.java
+++ src/main/java/com/gaaji/townlife/service/applicationservice/townlife/TownLifeFindServiceImpl.java
@@ -1,10 +1,10 @@
 package com.gaaji.townlife.service.applicationservice.townlife;
 
+import com.gaaji.townlife.global.utils.ULIDGenerator;
 import com.gaaji.townlife.service.controller.townlife.dto.TownLifeDetailDto;
 import com.gaaji.townlife.service.controller.townlife.dto.TownLifeListResponseDto;
 import com.gaaji.townlife.service.domain.townlife.TownLife;
 import com.gaaji.townlife.service.domain.townlife.TownLifeCounter;
-import de.huxhorn.sulky.ulid.ULID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Slice;
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Slf4j
 @Service
@@ -42,10 +41,10 @@ public class TownLifeFindServiceImpl implements TownLifeFindService {
 
     @Override
     @Transactional
-    public TownLifeListResponseDto findListByTownId(String townId, LocalDateTime requestTime, int page, int size) {
+    public TownLifeListResponseDto findListByTownId(String userId, String townId, LocalDateTime requestTime, int page, int size) {
 
-        String offsetId = new ULID().nextULID(requestTime.toInstant(ZoneOffset.UTC).toEpochMilli());
-        Slice<TownLife> townLives = entityService.findListByTownIdAndIdLessThan(townId, offsetId, page, size);
+        String offsetId = ULIDGenerator.newULIDByRequestTime(requestTime);
+        Slice<TownLife> townLives = entityService.findListByTownIdAndIdLessThan(userId, townId, offsetId, page, size);
 
         return TownLifeListResponseDto.of(townLives);
     }
@@ -54,7 +53,7 @@ public class TownLifeFindServiceImpl implements TownLifeFindService {
     @Transactional
     public TownLifeListResponseDto findListByUserId(String userId, LocalDateTime requestTime, int page, int size) {
 
-        String offsetId = new ULID().nextULID(requestTime.toInstant(ZoneOffset.UTC).toEpochMilli());
+        String offsetId = ULIDGenerator.newULIDByRequestTime(requestTime);
         Slice<TownLife> townLives = entityService.findListByUserIdAndIdLessThan(userId, offsetId, page, size);
 
         return TownLifeListResponseDto.of(townLives);
