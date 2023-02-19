785c4e10b145f4b0086f142f3f417c05b5f32fa2
Seungho <hsh47607@naver.com>
2023-02-19 17:22:17 +0900

    DTO : Reaction request/response DTO 정의

--- src/main/java/com/gaaji/townlife/service/controller/townlife/dto/ReactionDoResponseDto.java
+++ src/main/java/com/gaaji/townlife/service/controller/townlife/dto/ReactionDoResponseDto.java
@@ -1,4 +1,31 @@
 package com.gaaji.townlife.service.controller.townlife.dto;
 
+import com.gaaji.townlife.service.domain.reaction.Emoji;
+import lombok.*;
+
+@Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
 public class ReactionDoResponseDto {
+
+    private String townLifeId;
+    private String userId;
+    private Emoji emoji;
+
+    public static ReactionDoResponseDto of(String townLifeId, String userId) {
+        return ReactionDoResponseDto.builder()
+                .townLifeId(townLifeId)
+                .userId(userId)
+                .build();
+    }
+
+    public static ReactionDoResponseDto of(String townLifeId, String userId, Emoji emoji) {
+        return ReactionDoResponseDto.builder()
+                .townLifeId(townLifeId)
+                .userId(userId)
+                .emoji(emoji)
+                .build();
+    }
+
 }
