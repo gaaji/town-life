526403263b3b03c0834d84e8de553a3c8bb23451
Seungho <hsh47607@naver.com>
2023-02-20 16:51:05 +0900

    style: 메시지 변경

--- src/main/java/com/gaaji/townlife/global/exception/api/ApiErrorCode.java
+++ src/main/java/com/gaaji/townlife/global/exception/api/ApiErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ApiErrorCode implements ErrorCode {
-    TOWN_LIFE_NOT_FOUND(HttpStatus.NOT_FOUND, "TL-0001", "해당 동네생활을 찾을 수 없습니다."),
+    TOWN_LIFE_NOT_FOUND(HttpStatus.NOT_FOUND, "TL-0001", "해당 게시글을 찾을 수 없습니다."),
     TOWN_LIFE_SAVE_ERROR(HttpStatus.BAD_REQUEST, "TL-0002", "동네생활 게시글 등록에 실패하였습니다."),
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "TL-0003", "해당 카테고리를 찾을 수 없습니다."),
     AUTHORIZATION_MODIFY_ERROR(HttpStatus.UNAUTHORIZED, "TL-0004", "수정에 대한 권한이 없습니다."),
