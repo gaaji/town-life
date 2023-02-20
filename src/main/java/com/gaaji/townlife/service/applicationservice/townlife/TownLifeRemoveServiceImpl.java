e94f80f926251daa42e3935df74d85ec154a9ef4
Seungho <hsh47607@naver.com>
2023-02-20 16:53:24 +0900

    feat : 동네생활 삭제 로직 구현

--- src/main/java/com/gaaji/townlife/service/applicationservice/townlife/TownLifeRemoveServiceImpl.java
+++ src/main/java/com/gaaji/townlife/service/applicationservice/townlife/TownLifeRemoveServiceImpl.java
@@ -2,11 +2,12 @@ package com.gaaji.townlife.service.applicationservice.townlife;
 
 import com.gaaji.townlife.global.exception.api.ApiErrorCode;
 import com.gaaji.townlife.global.exception.api.ResourceNotFoundException;
-import com.gaaji.townlife.global.exception.api.ResourceRemoveException;
 import com.gaaji.townlife.service.domain.townlife.TownLife;
+import com.gaaji.townlife.service.repository.TownLifeCounterRepository;
 import com.gaaji.townlife.service.repository.TownLifeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.gaaji.townlife.global.utils.validation.ValidateResourceAccess.validateAuthorizationRemoving;
 
@@ -15,23 +16,18 @@ import static com.gaaji.townlife.global.utils.validation.ValidateResourceAccess.
 public class TownLifeRemoveServiceImpl implements TownLifeRemoveService {
 
     private final TownLifeRepository townLifeRepository;
+    private final TownLifeCounterRepository townLifeCounterRepository;
 
     @Override
+    @Transactional
     public void remove(String townLifeId, String authorId) {
         TownLife townLife = townLifeRepository.findById(townLifeId)
                 .orElseThrow(() -> new ResourceNotFoundException(ApiErrorCode.TOWN_LIFE_NOT_FOUND));
 
-        validateAlreadyRemoved(townLife);
-
         validateAuthorizationRemoving(authorId, townLife.getAuthorId());
 
+        townLifeCounterRepository.delete(townLife.getTownLifeCounter());
         townLifeRepository.delete(townLife);
     }
 
-    //TODO @Query 로 한다고 하지 않았나???
-    private void validateAlreadyRemoved(TownLife townLife) {
-        if( townLife.getDeletedAt() != null ) {
-            throw new ResourceRemoveException(ApiErrorCode.TOWN_LIFE_REMOVE_ERROR);
-        }
-    }
 }
--- src/main/java/com/gaaji/townlife/service/repository/TownLifeRepository.java
+++ src/main/java/com/gaaji/townlife/service/repository/TownLifeRepository.java
@@ -5,13 +5,18 @@ import com.gaaji.townlife.service.domain.townlife.TownLife;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TownLifeRepository extends JpaRepository<TownLife, String> {
 
-    Slice<TownLife> findByTownIdAndIdLessThan(String townId, String id, Pageable pageable);
-    Slice<TownLife> findByTownIdAndIdLessThanAndCategoryNotIn(String townId, String id, List<Category> excludedCategories, Pageable pageable);
-    Slice<TownLife> findByAuthorIdAndIdLessThan(String authorId, String id, Pageable pageable);
+    @Override
+    @Query(value = "SELECT t FROM TownLife t WHERE t.id = :id AND t.deletedAt is null")
+    Optional<TownLife> findById(String id);
+    Slice<TownLife> findByTownIdAndIdLessThanAndDeletedAtIsNull(String townId, String id, Pageable pageable);
+    Slice<TownLife> findByTownIdAndIdLessThanAndCategoryNotInAndDeletedAtIsNull(String townId, String id, List<Category> excludedCategories, Pageable pageable);
+    Slice<TownLife> findByAuthorIdAndIdLessThanAndDeletedAtIsNull(String authorId, String id, Pageable pageable);
 
 }
