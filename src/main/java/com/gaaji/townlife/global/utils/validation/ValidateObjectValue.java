a7102a18824ceaebd1edc700d1480564d97c2722
Seungho <hsh47607@naver.com>
2023-02-21 17:37:39 +0900

    feat: validate object is non null or non blank

---
+++ src/main/java/com/gaaji/townlife/global/utils/validation/ValidateObjectValue.java
@@ -0,0 +1,43 @@
+package com.gaaji.townlife.global.utils.validation;
+
+import com.gaaji.townlife.global.exceptions.internalServer.exception.NullValueException;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class ValidateObjectValue {
+
+    public static <T> void validateRequireNonNull(T obj) throws NullValueException {
+        try {
+            Objects.requireNonNull(obj);
+
+            if (obj instanceof String) {
+                validateRequireNonBlank((String) obj);
+            }
+        } catch (NullPointerException e) {
+            throw new NullValueException(e);
+        }
+    }
+
+    public static <T> void validateRequireNonNull(T... objs) throws NullValueException {
+        try {
+            for (T obj : objs) {
+                Objects.requireNonNull(obj);
+
+                if (obj instanceof String) {
+                    validateRequireNonBlank((String) obj);
+                }
+            }
+        } catch (NullPointerException e) {
+            throw new NullValueException(e);
+        }
+    }
+
+    private static void validateRequireNonBlank(String obj) throws NullValueException {
+        if(obj.isBlank() || obj.isEmpty()) {
+            throw new NullValueException();
+        }
+    }
+
+}
