1896f650eb0634fd60704f0f5b6fa0e2a57d7732
Seungho <hsh47607@naver.com>
2023-02-23 17:31:03 +0900

    add: Enum클래스 JSON 매핑 추가

--- src/main/java/com/gaaji/townlife/service/domain/townlife/TownLifeType.java
+++ src/main/java/com/gaaji/townlife/service/domain/townlife/TownLifeType.java
@@ -1,10 +1,32 @@
 package com.gaaji.townlife.service.domain.townlife;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.gaaji.townlife.global.exceptions.api.exception.BadRequestException;
 
 public enum TownLifeType {
-    @JsonProperty("post")
-    POST,
-    @JsonProperty("question")
-    QUESTION
+
+    POST("post"),
+    QUESTION("question");
+
+    private String value;
+
+    TownLifeType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static TownLifeType from(String value) {
+        for (TownLifeType type : TownLifeType.values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new BadRequestException("올바른 TownLifeType으로 요청해주세요. (ex. 'post' & 'question')");
+    }
 }
