cec2e587e9bb30fb34c5ff65f01d17ca67131b20
Seungho <hsh47607@naver.com>
2023-02-23 17:29:40 +0900

    add: handleApiException ApiInternalServerError 로그 추가

--- src/main/java/com/gaaji/townlife/global/exceptions/api/exception/RestControllerExceptionHandler.java
+++ src/main/java/com/gaaji/townlife/global/exceptions/api/exception/RestControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gaaji.townlife.global.exceptions.api.exception;
 
 import com.gaaji.townlife.global.exceptions.api.AbstractApiException;
 import com.gaaji.townlife.global.exceptions.api.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,10 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
+@Slf4j
 public class RestControllerExceptionHandler {
 
     @ExceptionHandler(AbstractApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(AbstractApiException e, HttpServletRequest request) {
+        if (e instanceof ApiInternalServerErrorException) {
+            log.error("Occurred Api Internal Server Exception");
+            e.printStackTrace();
+            if(e.getCause()!=null) {
+                e.getCause().printStackTrace();
+            }
+        }
         return ResponseEntity.status(e.getHttpStatus()).body(ErrorResponse.create(e, request.getRequestURI()));
     }
 }
