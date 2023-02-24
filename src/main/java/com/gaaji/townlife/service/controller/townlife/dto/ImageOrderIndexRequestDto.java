629311136a2639336d48fc20a9e6f9642fabe3eb
Seungho <hsh47607@naver.com>
2023-02-24 15:49:47 +0900

    DTO: create image order index request dto

---
+++ src/main/java/com/gaaji/townlife/service/controller/townlife/dto/ImageOrderIndexRequestDto.java
@@ -0,0 +1,14 @@
+package com.gaaji.townlife.service.controller.townlife.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageOrderIndexRequestDto {
+
+    private int[] orderIndexes;
+
+}
